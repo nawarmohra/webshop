57e2b7eb3f2a3f8847d35a715d4cec60c74719ed
fridaedda <frida.baldvinsdottir@gmail.com>
2020-06-09 14:17:29 +0200

    Form with Formik
    Form with Formik

--- my-app/src/components/App.tsx
+++ my-app/src/components/App.tsx
@@ -4,12 +4,19 @@ import React from 'react';
 import { products } from '../products';
 import { ProductList } from './ProductList'
 import Layout from './Layout'
+import { MyForm } from './MyForm'
 
 function App() {
   return (
     <div className="App">
-        <Layout />
+      <Layout />
+      <div style={{textAlign: "center"}}>
+        <MyForm onSubmit={({email, firstName, lastName, adress, postalCode}) => {
+          console.log(email, firstName, lastName, adress, postalCode)}} 
+        />
+      </div>
     </div>
+    
   );
 }
 
