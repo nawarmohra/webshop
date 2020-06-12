b7a2ba52b58ae49cd01739ac06aa8c1aff7dcb39
fridaedda <frida.baldvinsdottir@gmail.com>
2020-06-12 12:31:40 +0200

    roter checkout
    roter checkout

--- my-app/src/router/router.js
+++ my-app/src/router/router.js
@@ -3,6 +3,7 @@ import Layout from "../components/Layout";
 import SingleProduct from "../components/SingleProduct";
 import React from "react";
 import Cart from "../components/Cart";
+import Checkout from "../components/Checkout"
 
 export default function Router() {
   return (
@@ -10,6 +11,9 @@ export default function Router() {
       <Switch>
         <Route path="/cart">
           <Cart />
+        </Route>
+        <Route path="/checkout">
+          <Checkout />
         </Route>
         <Route path="/product/:id">
           <SingleProduct />
