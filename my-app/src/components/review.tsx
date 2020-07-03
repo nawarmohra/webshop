9f8acc79d387708486f4dd81663ae472db56bcd0
nawar mohra <nawarmohra@gmail.com>
2020-07-03 20:41:06 +0200

    xc

--- my-app/src/components/review.tsx
+++ my-app/src/components/review.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Grid from '@material-ui/core/Grid';
 import CartProducts from './cart-products';
 
 
